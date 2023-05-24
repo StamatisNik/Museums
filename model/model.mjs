a4c9f424dc67d77af93b54257b3745adab59a736
nicolenicolaou <119522218+nicolenicolaou@users.noreply.github.com>
2023-05-24 12:47:00 +0300

    Add files via upload

--- model/model.mjs
+++ model/model.mjs
@@ -1,136 +1,181 @@
-import mysql from "mysql2"
-import bcrypt from "bcrypt";
-import MySQLStore from "express-mysql-session";
-const connection = mysql.createConnection({
-    host: 'localhost', 
-    user: 'root', 
-    password: 'password', 
-    database: 'museum_database'});
-  
-      connection.connect(function(err) {
-        if (err) {
-          console.error('Error connecting to MySQL server: ' + err.message);
-          return;
-        }
-        console.log('Connected to MySQL server.');
-      });
-    
+import mysql from "mysql2/promise";
+import faker from "faker";
 
-      
-      async function insertDataIntoTable(values) {
-        try {
-          const sql = 'INSERT INTO ticket(ticket_type) VALUES (?)';
-          const result = await connection.promise().query(sql, values);
-          return result;
-        } catch (error) {
-          throw error;
-        }
-       
-      }
-        
-      
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  database: 'museum_database',
+  user: 'root',
+  password: 'password',
+  insecureAuth: true
+});
+
+const generateFakeEmail = (name, surname) => {
+  const domain = 'email.com';
+  const randomNumber = Math.floor(Math.random() * 1000);
+  const formattedName = name.toLowerCase();
+  const formattedSurname = surname.toLowerCase();
+  return `${formattedName}.${formattedSurname}${randomNumber}@${domain}`;
+};
 
 
-     async function checkEmailExists(email) {
-      try
-      {
-        const sql = 'SELECT email FROM visitor WHERE email = ?';
-        const result=await connection.promise().query(sql, [email]);
-        return result;
-      } catch (error) {
-        throw error;
-      }
-     }
-
-     async function retreivePassword(email) {
-      try
-      {
-        const sql = 'SELECT user_password FROM visitor WHERE email = ?';
-        const result=await connection.promise().query(sql, [email]);
-        return result;
-      } catch (error) {
-        throw error;
-      }
-     }
-  
-      
-     async function UserValidationRegister(firstname,lastname,email,confirmEmail,password,passwordConfirm,dateOfBirth, req, res) {
-      
-        let exists = await checkEmailExists(email);
-        console.log(exists[0]);
-    
-        if (exists[0].length > 0) {
-          req.session.registrationData = {
-            firstname,
-            lastname,
-            email,
-            confirmEmail,
-            dateOfBirth
-          };
-          return res.redirect("/register?error=true");
-        
-        }
-        try{
-          const hash = await bcrypt.hash(password, 10);
-          const sql = "INSERT INTO visitor(email, user_password) VALUES (?, ?)";
-          const result = await connection.promise().query(sql, [email, hash]);
-          res.redirect("/login");
-          return result;
-
-        }
-       catch (error) {
-        throw error
-      }
-     }
-    
-  
+const generateFakeTicketId = () => {
+    const prefix = 'TICKET-';
+    const randomNumber = Math.floor(Math.random() * 1000000);
+    const ticketNumber = prefix + randomNumber.toString().padStart(6, '0');
+    return ticketNumber;
+  };
   
 
-     async function UserValidationLogin(email, password, req, res) {
-      let exists = await checkEmailExists(email);
-      const userPassword = await retreivePassword(email);
-     
-      if (!exists[0].length) {
-        req.session.SignInData = {
-          email
-        };
-        return res.redirect("/login?email=invalid");
-      }
-     
-      
 
-      if (!userPassword[0][0].user_password) {
-        req.session.SignInData = {
-          email
-        };
-      
-        return res.redirect("/login?password=invalid");
-      }
-
-      const hashedPassword=userPassword[0][0].user_password;
-      try {
-        const isMatch = await bcrypt.compare(password, hashedPassword);
-        console.log(isMatch);
-        if (!isMatch) {
-          req.session.SignInData = {
-            email
-          };
-           return res.redirect("/login?password=invalid");
-        }
-          req.session.isAuth=true;
-          return res.redirect("/tickets");
-        }  
-        catch (error) {
-        throw error;
+  const mastercardCheck = /^(?:5[1-5][0-9]{14})$/;
+  const visaCheck = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+  const amexCheck = /^(?:3[47][0-9]{13})$/;
+  const dinersClubCheck = /^(?:3(?:0[0-5]|[68][0-9])[0-9]{11})$/;
+  
+  const cardNumberRegexes = [mastercardCheck, visaCheck, amexCheck, dinersClubCheck];
+  
+  const generateRandomCardNumber = () => {
+    const randomIndex = Math.floor(Math.random() * cardNumberRegexes.length);
+    const selectedRegex = cardNumberRegexes[randomIndex];
+    let cardNumber = '';
+  
+    for (let i = 0; i < 16; i++) {
+      if (selectedRegex.test(cardNumber + '0')) {
+        cardNumber += '0';
+      } else if (selectedRegex.test(cardNumber + '1')) {
+        cardNumber += '1';
+      } else {
+        cardNumber += Math.floor(Math.random() * 10).toString();
       }
     }
   
-      
+    return cardNumber;
+  };
 
-    
+  const exhibits = {
+    a: 20,
+    b: 35,
+    c: 45,
+    d: 60
+  };
   
-      
-   
-      
-    
-export {insertDataIntoTable,UserValidationRegister,UserValidationLogin};
+  const getRandomExhibit = () => {
+    const exhibitKeys = Object.keys(exhibits);
+    const randomIndex = Math.floor(Math.random() * exhibitKeys.length);
+    const randomExhibit = exhibitKeys[randomIndex];
+    return randomExhibit;
+  };
+  
+  const selectedExhibit = getRandomExhibit();
+  const selectedExhibitPrice = exhibits[selectedExhibit];
+
+  const countries = [
+    'Argentina',
+    'Brazil',
+    'Canada',
+    'Cyprus',
+    'Denmark',
+    'Egypt',
+    'France',
+    'Germany',
+    'Greece',
+    'Hungary',
+    'Italy',
+    'India',
+    'Japan',
+    'Kenya',
+    'Mexico',
+    'Norway',
+    'Portugal',
+    'Russia',
+    'Spain',
+    'Turkey',
+    'United Kingdom',
+    'United States',
+    'Zimbabwe'
+  ];
+  
+  const getRandomCountry = () => {
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    return countries[randomIndex];
+  };
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * (999 - 100 + 1)) + 100;
+  };
+
+  const getRandomFutureDate = () => {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const futureYear = Math.floor(Math.random() * (2027 - currentYear + 1)) + currentYear;
+    const futureMonth = String(Math.floor(Math.random() * 12) + 1).padStart(2, '0');
+    const futureDay = String(Math.floor(Math.random() * 28) + 1).padStart(2, '0');
+    const futureHours = String(Math.floor(Math.random() * 24)).padStart(2, '0');
+    const futureMinutes = String(Math.floor(Math.random() * 60)).padStart(2, '0');
+    const futureSeconds = String(Math.floor(Math.random() * 60)).padStart(2, '0');
+  
+    const futureDate = `${futureYear}-${futureMonth}-${futureDay} ${futureHours}:${futureMinutes}:${futureSeconds}`;
+  
+    return futureDate;
+  };
+
+  const getRandomTicketNumber = () => {
+    const min = 1;
+    const max = 99999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  
+
+  const getRandomPastDate = () => {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const pastYear = Math.floor(Math.random() * (currentYear - 2020 + 1)) + 2020;
+    const pastMonth = String(Math.floor(Math.random() * 12) + 1).padStart(2, '0');
+    const pastDay = String(Math.floor(Math.random() * 28) + 1).padStart(2, '0');
+    const pastHours = String(Math.floor(Math.random() * 24)).padStart(2, '0');
+    const pastMinutes = String(Math.floor(Math.random() * 60)).padStart(2, '0');
+    const pastSeconds = String(Math.floor(Math.random() * 60)).padStart(2, '0');
+  
+    const pastDate = `${pastYear}-${pastMonth}-${pastDay} ${pastHours}:${pastMinutes}:${pastSeconds}`;
+  
+    return pastDate;
+  };
+
+try {
+  // Generate fake data
+  for (let i = 0; i <100; i++) {
+  
+  const fakeName = faker.name.firstName();
+  const fakeSurname = faker.name.lastName();
+  const fakeEmail = generateFakeEmail(fakeName, fakeSurname);
+  const fakeCardNumber = generateRandomCardNumber();
+  const fakeTicketId = generateFakeTicketId();
+  const selectedCountry = getRandomCountry();
+  const randomFutureDate = getRandomFutureDate();
+  const randomPastDate = getRandomPastDate();
+  const randomNumber = getRandomNumber();
+  const selectedExhibit = getRandomExhibit();
+  const fakeTicketNumber = getRandomTicketNumber();
+  const selectedExhibitPrice = exhibits[selectedExhibit];
+
+  // Insert data into Visitor table
+  const insertVisitorQuery = `INSERT INTO Visitor (email, firstname, lastname, exhibit_name, card_no) VALUES (?, ?, ?, ?, ?)`;
+  await connection.execute(insertVisitorQuery, [fakeEmail, fakeName, fakeSurname, selectedExhibit, fakeCardNumber]);
+
+  // Insert data into Payment table
+  const insertPaymentQuery = `INSERT INTO Payment (card_no, card_owner, cvv, end_date) VALUES (?, ?, ?, ?)`;
+  await connection.execute(insertPaymentQuery, [fakeCardNumber, fakeName + ' ' + fakeSurname, randomNumber, randomFutureDate]);
+
+  // Insert data into Ticket table
+  const insertTicketQuery = `INSERT INTO Ticket (id, firstname, lastname, ticket_no, email_user, card_no, price, book_date, book_time, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  await connection.execute(insertTicketQuery, [fakeTicketId, fakeName, fakeSurname, fakeTicketNumber, fakeEmail, fakeCardNumber, selectedExhibitPrice, randomPastDate, randomFutureDate, selectedCountry]);
+
+  console.log(`Data ${i + 1} inserted successfully!`);}
+} catch (error) {
+  console.error('Error inserting data:', error);
+} finally {
+  connection.end();
+}
+
+export { connection };
---
+++ model/museum_database.sql
@@ -0,0 +1,37 @@
+USE museum_database; 
+CREATE TABLE IF NOT EXISTS  Visitor (
+	email varchar(255) NOT NULL PRIMARY KEY,
+	firstname varchar(255),
+	lastname varchar(255),
+    exhibit_name  varchar(255),
+    card_no varchar(255),
+    FOREIGN KEY (exhibit_name) REFERENCES Exhibit(exhibit_name),
+    FOREIGN KEY (card_no) REFERENCES Payment(card_no)
+);
+CREATE TABLE IF NOT EXISTS  Payment (
+	card_no varchar(255) NOT NULL PRIMARY KEY,
+	card_owner varchar(255),
+	cvv varchar(255),
+	end_date datetime
+);
+
+CREATE TABLE IF NOT EXISTS Ticket (
+	id varchar(255) NOT NULL PRIMARY KEY,
+	firstname varchar(255),
+	lastname varchar(255),
+	ticket_no varchar(255),
+    email_user varchar(255),
+    card_no varchar(255),
+	price real,
+    book_date datetime,
+	book_time datetime,
+	country varchar(255),
+    FOREIGN KEY (email_user) REFERENCES Visitor(email),
+    FOREIGN KEY (card_no) REFERENCES Payment(card_no)
+);
+
+CREATE TABLE IF NOT EXISTS  Exhibit (
+	exhibit_name varchar(255) NOT NULL PRIMARY KEY,
+	price real
+);
+
