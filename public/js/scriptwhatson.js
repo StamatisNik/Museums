078e4fbbc8915bb89da39fe5d9eb2615fe102e11
StamatisNik <119521573+StamatisNik@users.noreply.github.com>
2023-05-28 18:44:10 +0300

    Delete public directory

--- public/js/scriptwhatson.js
+++
@@ -1,2 +0,0 @@
-let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
-let popoverList = popoverTriggerList.map( function(popoverTrigger){ return new bootstrap.Popover(popoverTrigger);});
